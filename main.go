0a58e9da71ae08b8444107ba20a5ce233d7c1877
Shahid <shahid@epixelsolutions.com>
2023-11-24 13:46:24 +0530

    Modified the code

--- main.go
+++ main.go
@@ -8,37 +8,39 @@ import (
 )
 
 func main() {
-  err := sentry.Init(sentry.ClientOptions{
-    Dsn: "https://3cc86389ab265486c0a4801fb15968ef@o4506279358496768.ingest.sentry.io/4506279368720384",
-    // Set TracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production,
-    TracesSampleRate: 1.0,
-  })
-  if err != nil {
-    log.Fatalf("sentry.Init: %s", err)
-  }
-
-  // Flush buffered events before the program terminates.
-  defer sentry.Flush(5 * time.Second)
-  err = checkTest()
-  err2 := "DummyError"
-  log.Println(err)
-
-  sentry.ConfigureScope(func(scope *sentry.Scope) {
-	  scope.SetLevel(sentry.LevelError)
-	//   sentry.CaptureException(err2)
-	  sentry.CaptureMessage(err2)
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://3cc86389ab265486c0a4801fb15968ef@o4506279358496768.ingest.sentry.io/4506279368720384",
+		Debug : true,
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
 	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
 
-}
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
 
-func checkTest()error{
-	err := checkTest2()
-	return err
-}
+	err = errors.New("Test Error. It's Working.")
+	sentry.WithScope(func(scope *sentry.Scope) {
 
-func checkTest2()error{
-	err := errors.New("Check Error from checkTest22")
-	return err
+		// set context character
+		scope.SetContext("character", map[string]interface{}{
+			"name":        "Mighty Fighter",
+			"age":         19,
+			"attack_type": "melee",
+		})
+
+		//add extra data additional
+		scope.SetExtra("extra_key", "extra_value")
+		//set user id
+		scope.SetUser(sentry.User{ID: "test_user"})
+
+		scope.SetLevel(sentry.LevelError)
+
+		sentry.CaptureException(err)
+	})  
+
 }
